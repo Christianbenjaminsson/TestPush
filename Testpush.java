0e7c3e63b408f4148282f86bfa5d64842870e217
Christianbenjaminsson <christianbenjaminsson@outlook.com>
2019-11-05 21:37:32 +0100

    Test to push

---
+++ Testpush.java
@@ -0,0 +1,8 @@
+package com.example.testpush;
+
+public class Testpush {
+
+    public static void main(String[] args) {
+        System.out.println("This is just a test to see if it works to push to Github");
+    }
+}
